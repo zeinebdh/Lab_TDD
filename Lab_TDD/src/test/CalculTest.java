2f22451d4b541f3e746f655071fc34f66c641f13
Zeineb <Zeineb@DESKTOP-5SM3HJ7>
2024-04-03 16:10:46 +0100

    Second Test

--- Lab_TDD/src/test/CalculTest.java
+++ Lab_TDD/src/test/CalculTest.java
@@ -15,4 +15,18 @@ public class CalculTest {
 	        Calculator calculator = new Calculator();
 	        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> calculator.add(-1, 2));
 	        assertEquals("Inputs cannot be negative.", exception.getMessage());}
+	  
+	  //When adding 2 positive integers
+	  //Then display the accurate result of the addition of the 2 inputs
+	  @Test
+	   public  void testAdd_PositiveIntegers() {
+	        Calculator calculator = new Calculator();
+	        assertEquals(5, calculator.add(2, 3));
+	    }
+	  
+	  
+	  
+	  
+	  
+	  
 }
